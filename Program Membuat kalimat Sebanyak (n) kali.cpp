df14f6d37ce105253f2ad319602a5bfad27f50b6
Firentus Agustone Pasaribu <45529731+firentus999@users.noreply.github.com>
2018-12-09 17:18:03 +0700

    programnya ..

---
+++ Program Membuat kalimat Sebanyak (n) kali.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<" Masukan jumlah yang diinginkan=\n";
+    cin>>n;
+    cout<<" hasilnya\n\n"<<endl;
+    for(int a=0;a<n;a++)
+        cout<<"Algoritma dan Pemograman\n ";
+    return 0;
+}
